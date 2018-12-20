8acf3c440e2091e2c69d63d943f1353b77e64e88
AlexanderBrez <jztenk@gmail.com>
2018-12-20 15:30:43 +0300

    => functions

--- ES6.js
+++ ES6.js
@@ -1,5 +1,19 @@
 'use strict';
 
+
+// Пременные ES6
 for (let i = 0; i <= 10; i++) {
   console.log(i);
 }
+
+// Стрелочные функции
+const cleanPhoneNumber = phone => phone.replace(/-/g, '');
+console.log(cleanPhoneNumber('8-962-915-55-86'));
+
+const anythingToLowerCase = anything => {
+  if (!anything) {
+    anything = '';
+  }
+  return anything.toLowerCase();
+};
+console.log(anythingToLowerCase());
