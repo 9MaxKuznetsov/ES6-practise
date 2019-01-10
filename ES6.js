708477553a847ce353503a252038afa6b9f5e143
AlexanderBrez <jztenk@gmail.com>
2019-01-10 18:58:31 +0300

    babel

--- ES6.js
+++ ES6.js
@@ -3,21 +3,34 @@
 
 // Пременные ES6
 for (let i = 0; i <= 10; i++) {
-  console.log(i);
+  console.log(i)
 }
 
 // Стрелочные функции
-const cleanPhoneNumber = phone => phone.replace(/-/g, '');
-console.log(cleanPhoneNumber('8-962-915-55-86'));
+const cleanPhoneNumber = phone => phone.replace(/-/g, '')
+console.log(cleanPhoneNumber('8-962-915-55-86'))
 
 const anythingToLowerCase = anything => {
   if (!anything) {
-    anything = '';
+    anything = ''
   }
-  return anything.toLowerCase();
-};
-console.log(anythingToLowerCase());
+  return anything.toLowerCase()
+}
+console.log(anythingToLowerCase('MaKe Me LoWeR pLeAsE'))
 
 // Шаблонные строки 
-const Pi = `Число Pi равно ${Math.PI}`;
-console.log(Pi);
+const Pi = `Число Pi равно ${Math.PI}`
+console.log(Pi)
+
+// Промисы
+const sleep = time => {
+  return new Promise(resolve => {
+    const data = `I'm waking up`
+    
+    setTimeout(() => {
+      resolve(data)
+    }, time)
+  })
+}
+
+console.log(sleep(1000))
